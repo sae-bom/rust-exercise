83d2f0e124b47c6977134561869596bf4ea564b3
sae-bom <saebom.kim@cluml.com>
2025-04-23 13:39:39 +0900

    Solve problem '2235. Add Two Integers'

---
+++ add-two-integers_2235/src/lib.rs
@@ -0,0 +1,27 @@
+struct Solution;
+
+impl Solution {
+    pub fn sum(num1: i32, num2: i32) -> i32 {
+        num1 + num2
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sum() {
+        assert_eq!(Solution::sum(-10, 4), -6);
+    }
+
+    #[test]
+    fn test_sum_upper_bound() {
+        assert_eq!(Solution::sum(100, 100), 200);
+    }
+
+    #[test]
+    fn test_sum_lower_bound() {
+        assert_eq!(Solution::sum(-100, -100), -200);
+    }
+}
