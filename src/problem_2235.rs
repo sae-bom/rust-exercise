6bae639d1f95b706c668c82d4c96a507a3f829c2
sae-bom <saebom.kim@cluml.com>
2025-04-29 10:17:09 +0900

    Remove Solution struct to keep the code simple

--- src/problem_2235.rs
+++ src/problem_2235.rs
@@ -1,9 +1,5 @@
-struct Solution;
-
-impl Solution {
-    pub fn sum(num1: i32, num2: i32) -> i32 {
-        num1 + num2
-    }
+pub fn sum(num1: i32, num2: i32) -> i32 {
+    num1 + num2
 }
 
 #[cfg(test)]
@@ -12,16 +8,16 @@ mod tests {
 
     #[test]
     fn test_sum() {
-        assert_eq!(Solution::sum(-10, 4), -6);
+        assert_eq!(sum(-10, 4), -6);
     }
 
     #[test]
     fn test_sum_upper_bound() {
-        assert_eq!(Solution::sum(100, 100), 200);
+        assert_eq!(sum(100, 100), 200);
     }
 
     #[test]
     fn test_sum_lower_bound() {
-        assert_eq!(Solution::sum(-100, -100), -200);
+        assert_eq!(sum(-100, -100), -200);
     }
 }
