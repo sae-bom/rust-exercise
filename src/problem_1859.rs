b88492951a223cac2175020d3c230cae58d91c41
sae-bom <saebom.kim@cluml.com>
2025-05-13 16:50:55 +0900

    Remove unsafe indexing syntax

--- src/problem_1859.rs
+++ src/problem_1859.rs
@@ -1,11 +1,18 @@
 #[allow(dead_code)]
 pub fn sort_sentence(s: &str) -> String {
+    if s.len() <= 1 {
+        return String::new();
+    }
     let mut sorted_words = s.split_whitespace().collect::<Vec<&str>>();
     sorted_words.sort_unstable_by_key(|str| str.chars().last());
 
     sorted_words
         .iter()
-        .map(|&str| &str[..str.len() - 1])
+        .map(|&str| {
+            str.get(..str.len() - 1).expect(
+                "It is always within the valid range, given that s.length >= 2 is guaranteed.",
+            )
+        })
         .collect::<Vec<&str>>()
         .join(" ")
 }
