31c489050403a578e37616dda72da1a65bf97380
Spring <sae-bom@users.noreply.github.com>
2025-05-20 13:19:39 +0900

    Remove unsafe indexing syntax

--- src/problem_1470.rs
+++ src/problem_1470.rs
@@ -1,11 +1,17 @@
 #[allow(dead_code)]
-pub fn shuffle(nums: &[i32], n: i32) -> Vec<i32> {
-    let n: usize = n.try_into().unwrap();
-    nums[..n]
+pub fn shuffle(nums: &[i32]) -> Result<Vec<i32>, &str> {
+    if nums.is_empty() || nums.len() > 500 || nums.len() % 2 != 0 {
+        return Err("Invalid input: nums.len() must be even and in the range 1 to 500 inclusive");
+    }
+
+    let n: usize = nums.len() / 2;
+    Ok(nums
+        .get(..n)
+        .expect("1 <= n < nums.length")
         .iter()
-        .zip(&nums[n..])
+        .zip(nums.get(n..).expect("1 <= n < nums.length"))
         .flat_map(|(&x, &y)| [x, y])
-        .collect()
+        .collect())
 }
 
 #[cfg(test)]
@@ -14,7 +20,7 @@ mod tests {
 
     #[test]
     fn example_1() {
-        let result = shuffle(&[2, 5, 1, 3, 4, 7], 3);
-        assert_eq!(result, vec![2, 3, 5, 4, 1, 7]);
+        let result = shuffle(&[2, 5, 1, 3, 4, 7]);
+        assert_eq!(result, Ok(vec![2, 3, 5, 4, 1, 7]));
     }
 }
--- src/problem_2160.rs
+++ src/problem_2160.rs
@@ -2,7 +2,13 @@
 pub fn minimum_sum(num: i32) -> i32 {
     let mut digits = [num / 1000, num / 100 % 10, num / 10 % 10, num % 10];
     digits.sort_unstable();
-    (digits[0] + digits[1]) * 10 + digits[2] + digits[3]
+    let [
+        smaller_digit_1,
+        smaller_digit_2,
+        bigger_digit_1,
+        bigger_digit_2,
+    ] = &digits;
+    (smaller_digit_1 + smaller_digit_2) * 10 + bigger_digit_1 + bigger_digit_2
 }
 
 #[cfg(test)]
