1eb2c7c3233c3b667dbd72d33175ed7d86812433
sae-bom <saebom.kim@cluml.com>
2025-05-28 13:28:20 +0900

    Use simpler code and more efficient data structure

--- src/problem_771.rs
+++ src/problem_771.rs
@@ -1,13 +1,21 @@
-#[allow(dead_code)]
-pub fn num_jewels_in_stones(jewels: &str, stones: &str) -> i32 {
-    let jewels: Vec<char> = jewels.chars().collect();
+use std::collections::HashSet;
 
-    stones.chars().map(|x| i32::from(jewels.contains(&x))).sum()
+#[allow(dead_code)]
+pub fn num_jewels_in_stones(jewels: &str, stones: &str) -> usize {
+    let jewels: HashSet<char> = jewels.chars().collect();
+
+    stones.chars().filter(|x| jewels.contains(x)).count()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn example_1() {
+        let result = num_jewels_in_stones("aA", "aAAbbbb");
+        assert_eq!(result, 3);
+    }
 
     #[test]
     fn empty_jewels() {
