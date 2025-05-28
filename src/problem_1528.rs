0f6e235505e098e38c98fb3849d2a1702d63591a
Spring <sae-bom@users.noreply.github.com>
2025-05-28 19:10:09 +0900

    Remove unnecessary memory allocation

--- src/problem_1528.rs
+++ src/problem_1528.rs
@@ -2,5 +2,5 @@
 pub fn restore_string(s: &str, indices: Vec<i32>) -> String {
     let mut index_char_pair: Vec<(i32, char)> = std::iter::zip(indices, s.chars()).collect();
     index_char_pair.sort_by_key(|&(i, _)| i);
-    String::from_iter::<Vec<char>>(index_char_pair.iter().map(|&(_, c)| c).collect())
+    index_char_pair.iter().map(|&(_, c)| c).collect()
 }
