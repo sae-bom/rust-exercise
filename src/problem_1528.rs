17f50940bb5965451d641417979f3fb24da85996
Spring <sae-bom@users.noreply.github.com>
2025-05-29 16:06:17 +0900

    Use unstable sort to improve performance

--- src/problem_1528.rs
+++ src/problem_1528.rs
@@ -1,6 +1,6 @@
 #[allow(dead_code)]
 pub fn restore_string(s: &str, indices: Vec<i32>) -> String {
     let mut index_char_pair: Vec<(i32, char)> = std::iter::zip(indices, s.chars()).collect();
-    index_char_pair.sort_by_key(|&(i, _)| i);
+    index_char_pair.sort_unstable_by_key(|&(i, _)| i);
     index_char_pair.iter().map(|&(_, c)| c).collect()
 }
